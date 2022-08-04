7737cd4e4de7281f2401d19253c6dbdd677fb3b5
lkidszzzz <yanjiang.cst@outlook.com>
2022-08-04 16:34:31 +0800

    函数式编程

--- Part4/02.functional/adder/adder.go
+++ Part4/02.functional/adder/adder.go
@@ -2,34 +2,30 @@ package main
 
 import "fmt"
 
-func adder1() func(int) int {
-	sum := 0
-	//闭包{函数体[局部变量+自由变量(...树)]}
-	return func(x int) int {
-		sum += x
-		return sum
-	}
-}
+//func adder() func(int) int {
+//	sum := 0
+//	return func(v int) int {
+//		sum += v
+//		return sum
+//	}
+//}
 
-//"正统函数式编程"(不能有变量)
+//正统函数式编程：
+
 type iAdder func(int) (int, iAdder)
 
-func adder2(base int) iAdder {
-	return func(x int) (int, iAdder) {
-		return base + x, adder2(base + x)
+func adder0(base int) iAdder {
+	return func(v int) (int, iAdder) {
+		return base + v, adder0(base + v)
 	}
 }
 
 func main() {
-	a1 := adder1()
-	for i := 0; i < 10; i++ {
-		fmt.Printf("0 + 1 + ... + %d = %d\n", i, a1(i))
-	}
-
-	a2 := adder2(0)
+	a := adder0(0)
 	for i := 0; i < 10; i++ {
 		var s int
-		s, a2 = a2(i)
-		fmt.Printf("0 + 1 + ... + %d = %d\n", i, s)
+		s, a = a(i)
+		fmt.Printf("0 + 1 + ... + %d = %d\n",
+			i, s)
 	}
 }
