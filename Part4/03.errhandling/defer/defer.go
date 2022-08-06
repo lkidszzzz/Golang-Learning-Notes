120bd98c6fc26f72a4370e7552c589d2220b1381
lkidszzzz <yanjiang.cst@outlook.com>
2022-08-06 16:46:00 +0800

    错误处理

--- Part4/03.errhandling/defer/defer.go
+++ Part4/03.errhandling/defer/defer.go
@@ -35,9 +35,19 @@ func trydefer2() {
 }
 
 func writeFile(filename string) {
-	file, err := os.Create(filename)
+	file, err := os.OpenFile(filename,
+		os.O_EXCL|os.O_CREATE|os.O_WRONLY, 0666)
+
 	if err != nil {
-		panic(err)
+		if pathError, ok := err.(*os.PathError); !ok {
+			panic(err)
+		} else {
+			fmt.Printf("%s, %s, %s\n",
+				pathError.Op,
+				pathError.Path,
+				pathError.Err)
+		}
+		return
 	}
 	defer file.Close()
 
