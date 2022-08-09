d4b57c36adde0aba5af7cd8ec445cf74e222c310
lkidszzzz <yanjiang.cst@outlook.com>
2022-08-09 22:37:17 +0800

    并发

---
+++ Part4/05.channel/channel.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// 不要通过共享内存来通信，通过通信来共享内存
+
+//ch <- x // 发送
+//x = <-ch // 接收
+//<-ch // 接收，丢弃结果
+//close(ch) // 关闭
+
+func worker(id int, c chan int) {
+	for n := range c {
+		fmt.Printf("Worker %d received %c\n",
+			id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
+func chanDemo() {
+	var channels [10]chan<- int
+	for i := 0; i < 10; i++ {
+		channels[i] = createWorker(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + i
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'A' + i
+	}
+
+	time.Sleep(time.Millisecond)
+}
+
+func bufferedChannel() {
+	c := make(chan int, 3)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	time.Sleep(time.Millisecond)
+}
+
+func channelClose() {
+	c := make(chan int)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	close(c)
+	time.Sleep(time.Millisecond)
+}
+
+func main() {
+	fmt.Println("Channel as first-class citizen")
+	chanDemo()
+	fmt.Println("Buffered channel")
+	bufferedChannel()
+	fmt.Println("Channel close and range")
+	channelClose()
+}
